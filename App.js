43f1f5d96277164007d5a4b1778745bdf7d65987
devJeremie <chabanaisjeremie@gmail.com>
2024-04-29 16:26:40 +0200

    ShoppingListSTudent

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput 
+          style={styles.textInput}
+        />
+        <Button 
+          title='Valider'
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   padding:40,
+   paddingTop: 60
   },
+  inputContainer: {
+    flexDirection: "row",
+  },
+  textInput: {
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 5,
+    paddingLeft: 9,
+    fontSize: 18,
+    flexGrow: 1,
+  }
 });
