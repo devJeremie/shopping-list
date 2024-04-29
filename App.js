d5c4a528e574b6c0bcc2e22c49ad59c17ee3a436
devJeremie <chabanaisjeremie@gmail.com>
2024-04-29 21:36:13 +0200

    InputProducts

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { 
+  StyleSheet, Text,
+  View, TextInput, Button } from 'react-native';
+import React, { useState } from "react";
 
 export default function App() {
+
+  const [product, setProduct] = useState("");
+  const [myProducts, setMyProducts] = useState([]);
+
+  const inputHandler = (val) => { // input handler for product name text field
+    setProduct(val)
+  }
+
+  const submitHandler = () => { //enregistre les new products dans l'array du state myproducts
+    setMyProducts(currentMyProducts => [...currentMyProducts, product] ) //les produits existants et on les injectent dans un nouvel array
+    setProduct('')
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput 
           style={styles.textInput}
+          placeholder='Nouveau produit'
+          onChangeText={inputHandler} //permet de modifier la valeur de départ que vous avez mis dans le satet('' par défaut)
+          value={product}
         />
         <Button 
           title='Valider'
+          onPress={submitHandler}
         />
       </View>
     </View>
