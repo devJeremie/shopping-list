1aaa188a0092bda07baa825b01e529438358beab
devJeremie <chabanaisjeremie@gmail.com>
2024-05-17 18:56:12 +0200

    BottomTabsNavigation

---
+++ components/navigation/AppNavigation.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {NavigationContainer} from "@react-navigation/native"
+
+const AppNavigation = () => {
+  return (
+    <View>
+      <Text>AppNavigation</Text>
+    </View>
+  )
+}
+
+export default AppNavigation
+
+const styles = StyleSheet.create({
+
+})
